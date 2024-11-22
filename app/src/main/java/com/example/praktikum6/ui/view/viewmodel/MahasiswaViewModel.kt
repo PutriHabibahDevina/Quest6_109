ccb27ff9459ad47172fc3620dc0606bdf045307c
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-22 17:22:20 +0700

    Menambahkan fungsi setMatakuliah yang berisi elemen namamk dan kelas yang bisa dimodifikasi inputannya

--- app/src/main/java/com/example/praktikum6/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -7,19 +7,29 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class MahasiswaViewModel : ViewModel(){
+class MahasiswaViewModel : ViewModel() {
     //Request
     private val _statusUI = MutableStateFlow(Mahasiswa())
 
     val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
 
-    fun setMahasiswa(ls: MutableList<String>){
+    fun setMahasiswa(ls: MutableList<String>) {
         _statusUI.update { statusSaatIni ->
             statusSaatIni.copy(
                 nim = ls[0],
                 nama = ls[1],
                 email = ls[2]
             )
+        }
+    }
+
+    fun setMatakuliah(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                namamk = ls[0],
+                kelas = ls[1],
+            )
+
         }
     }
 }
