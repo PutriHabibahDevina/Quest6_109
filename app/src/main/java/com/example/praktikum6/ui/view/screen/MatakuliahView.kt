1eb3600327344af85d2a5d3a15c79f1980a83fbd
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-22 16:53:20 +0700

    Menambahkan box dengan background putih yang berisi teks dan textfield untuk menginput data

--- app/src/main/java/com/example/praktikum6/ui/view/screen/MatakuliahView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screen/MatakuliahView.kt
@@ -76,6 +76,44 @@ fun MatakuliahView(
                     fontWeight = FontWeight.Light
                 )
             }
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            Box(modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(topEnd = 15.dp, topStart = 15.dp)
+                )
+                .fillMaxSize()
+            ){
+                Column (
+                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally //Supaya tulisan di tengah
+                ){
+                    Text(text = "Masukkan Data Kamu", fontWeight = FontWeight.Bold, fontSize = 19.sp)
+                    Text(text = "Isi sesuai data yang kamu daftarkan", fontWeight = FontWeight.Light)
+
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = namamk,
+                        onValueChange = {namamk = it},
+                        label = { Text(text = "Nama Matakuliah") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Info,
+                                contentDescription = ""
+                            )
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next
+                        ),
+                        singleLine = true,
+                        shape = RoundedCornerShape(50.dp)
+
+                    )
+
+                }
+            }
         }
     }
 }
