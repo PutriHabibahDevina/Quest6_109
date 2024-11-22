f3d55f9b0f57681591867711b83d1df3c139dfb5
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-22 17:05:57 +0700

    Membuat kolom untuk menampilkan data nim, nama, dan email

--- app/src/main/java/com/example/praktikum6/ui/view/screen/TampilView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screen/TampilView.kt
@@ -1,10 +1,14 @@
 package com.example.praktikum6.ui.view.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +17,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.praktikum6.R
 import com.example.praktikum6.ui.model.Mahasiswa
+
 
 
 @Composable
@@ -29,7 +34,23 @@ fun TampilView(
                 .background(Color.White,  shape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
                 .padding(32.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-        ){
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                ) {
+                    Text("Nim:")
+                    Text(uiState.nim)
+                    Text("Nama:")
+                    Text(uiState.nama)
+                }
+                Column(
+                ) {
+                    Text(uiState.email)
+                }
+            }
 
         }
     }
