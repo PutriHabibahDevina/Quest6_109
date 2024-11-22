0eb060b9f96d17d38f708103337fc7019a3be65a
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-22 17:03:40 +0700

    Membuat kolom kedua dengan background warna putih

--- app/src/main/java/com/example/praktikum6/ui/view/screen/TampilView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screen/TampilView.kt
@@ -3,12 +3,17 @@ package com.example.praktikum6.ui.view.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.praktikum6.R
 import com.example.praktikum6.ui.model.Mahasiswa
+
 
 @Composable
 fun TampilView(
@@ -19,6 +24,13 @@ fun TampilView(
             .fillMaxSize().background(colorResource(id = R.color.primary)),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Column (
+            modifier = Modifier.fillMaxSize()
+                .background(Color.White,  shape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp))
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
 
+        }
     }
 }
