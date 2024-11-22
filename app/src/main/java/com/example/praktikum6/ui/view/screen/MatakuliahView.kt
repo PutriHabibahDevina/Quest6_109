c1e2571ccf8bd59fd8f6ea1e61611e1bcd64c4b5
PutriHabibahDevina <putrihdm13@gmail.com>
2024-11-22 16:45:12 +0700

    Menambahkan gambar logo umy, teks, serta mengatur warna background menggunakan row dan column

--- app/src/main/java/com/example/praktikum6/ui/view/screen/MatakuliahView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screen/MatakuliahView.kt
@@ -1,11 +1,42 @@
 package com.example.praktikum6.ui.view.screen
 
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.praktikum6.R
 import com.example.praktikum6.ui.model.Mahasiswa
 
 @Composable
@@ -19,4 +50,32 @@ fun MatakuliahView(
 
     var listData : MutableList<String> = mutableListOf(namamk, kelas)
 
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .background(color = colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row (verticalAlignment = Alignment.CenterVertically)
+        {
+            Image(
+                painter = painterResource(id = R.drawable.logo_umy),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.size(10.dp)) //Ngasih jarak antara gambar sama teks
+            Column{
+                Text(text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+    }
 }
